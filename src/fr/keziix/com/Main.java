53f99386192d0aa8f93a55ad4806ac67086ce55e
Xelow <XELOXLEGRANDE@gmail.com>
2021-05-17 02:21:21 +0200

    Coucou Keziix :)

---
+++ src/fr/keziix/com/Main.java
@@ -0,0 +1,31 @@
+package fr.keziix.com;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public static Main INSTANCE;
+
+    public SqlMaisLautre mysql = new SqlMaisLautre();
+
+    @Override
+    public void onEnable()
+    {
+        INSTANCE = this;
+        registerListeners();
+        registerCommands();
+        mysql.connect("localhost", "uwu", 3306, "root", "");
+    }
+
+    @Override
+    public void onDisable() {
+        mysql.disconnect();
+    }
+
+    private void registerListeners(){PluginManager pm = Bukkit.getPluginManager(); }
+
+    private void registerCommands(){ }
+
+}
