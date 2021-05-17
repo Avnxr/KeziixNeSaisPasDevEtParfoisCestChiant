71d8a20546391197085d3cb2ff968d08fcf16d36
Xelow <XELOXLEGRANDE@gmail.com>
2021-05-17 02:22:59 +0200

    Ca va ? =)

--- src/fr/keziix/com/Main.java
+++ src/fr/keziix/com/Main.java
@@ -4,7 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin
+{
 
     public static Main INSTANCE;
 
